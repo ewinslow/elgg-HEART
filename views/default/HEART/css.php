a7d42d855502aaa6859834fec5a68dc232405e82
Evan Winslow <evan.b.winslow@gmail.com>
2011-03-03 21:25:29 -0800

    Adjusting footer styles

--- views/default/HEART/css.php
+++ views/default/HEART/css.php
@@ -12,11 +12,22 @@ body {
 .elgg-page-default > .elgg-page-header { background: none}
 .elgg-page-default > .elgg-page-header > .elgg-inner {height: auto;padding: 0 0 30px}
 
-.elgg-page-default .elgg-page-body > .elgg-inner {
+.elgg-page-default > .elgg-page-body,
+.elgg-page-default > .elgg-page-footer {
+	width: 990px;
+	margin: 0 auto;
+}
+
+.elgg-page-default > .elgg-page-body > .elgg-inner {
 	background: #f5e7b6;
 	border-radius: 3px 3px 0 0;
+	width: auto;
+	padding: 5px;
 }
-.elgg-page-default .elgg-page-footer > .elgg-inner {
+
+.elgg-page-default > .elgg-page-footer > .elgg-inner {
+	width: auto;
+	padding: 10px;
 	border-radius: 0 0 3px 3px;
 	background: #f5e7b6;
 }
