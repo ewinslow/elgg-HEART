3c42881b113aa44f28ec652ddc849cd1143cd522
Evan Winslow <evan.b.winslow@gmail.com>
2011-03-03 21:26:43 -0800

    Adjusted footer

--- views/default/HEART/footer.php
+++ views/default/HEART/footer.php
@@ -1,3 +1,7 @@
-<p class="clearfloat center mtl"><small>
-HEART is a registered TRUST in Kenya and has a fully qualified board of Kenyan Directors who work with the American counterpart of HEART a 501 C-3 organization comprised of American health professionals and concerned business leaders.
-</small></p>
+<p class="clearfloat center mtl">
+	<small>
+		HEART is a registered TRUST in Kenya and has a fully qualified board of Kenyan Directors
+		who work with the American counterpart of HEART a 501 C-3 organization comprised of American
+		health professionals and concerned business leaders.
+	</small>
+</p>
