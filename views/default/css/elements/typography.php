75add5546ea6c9f14a820a94e076a903c02c1ca0
Evan Winslow <evan@elgg.org>
2012-08-19 22:21:15 -0700

    Motto heading font-weight toned down

--- views/default/css/elements/typography.php
+++ views/default/css/elements/typography.php
@@ -29,20 +29,24 @@ pre, code {
 	font-family: Monaco, "Courier New", Courier, monospace;
 	font-size: 12px;
 }
+
 input, textarea {
 	font: 120% Arial, Helvetica, sans-serif;
 }
+
 blockquote {
 	line-height: 1.3em;
 	margin-left: 40px;
 	margin-right: 40px;
 }
+
 h1, h2, h3, h4, h5, h6 {
 	color: #7D0B00;
 	font-family: "Trebuchet MS", "Times New Roman", serif;	
 	font-weight: bold;
 	margin-bottom: 0.5em;
 }
+
 h1 { font-size: 2em; }
 h2 { font-size: 1.8em; }
 h3 { font-size: 1.2em; }
@@ -79,17 +83,20 @@ h1.elgg-heading-site {
 	padding: 8px 0 6px;
 	text-align: center;
 }
+
 .elgg-heading-basic {
 	color: #0054A7;
 	font-size: 1.2em;
 	font-weight: bold;
 }
+
 .HEART-heading-motto {
 	background: #444;
 	background: -webkit-gradient(linear, left bottom, left top, from(#333), to(#555));
 	color: #F5E7B6;
 	font-family: "Colonna MT Regular";
 	font-size: 24px;
+	font-weight: normal;
 	margin: 0;
 	padding: 8px 0 6px;
 	text-align: center;
