be552038c421cc94cbf938d392ef84c99cb72b6d
Evan Winslow <evan@elgg.org>
2012-12-02 17:17:58 -0800

    Try getting around engine/start.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 $plugin_root = dirname(__DIR__);
 
-//require_once "$plugin_root/Elgg/engine/start.php";
+require_once "$plugin_root/Elgg/engine/lib/elgglib.php";
+require_once "$plugin_root/Elgg/engine/lib/autoloader.php";
 
-//elgg_register_classes("$plugin_root/classes");
+elgg_register_classes("$plugin_root/classes");
