884928b365d53470c9a30fb4258efa4fe67b8966
Evan Winslow <evan@elgg.org>
2012-12-02 17:05:18 -0800

    Some fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
-require_once dirname(__DIR__) . '/Elgg/engine/start.php';
+$plugin_root = dirname(__DIR__);
+
+require_once "$plugin_root/Elgg/engine/start.php";
+
+elgg_register_classes("$plugin_root/classes");
