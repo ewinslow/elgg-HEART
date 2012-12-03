6620b809b3859bbc66838901c9af63d82cd924a2
Evan Winslow <evan@elgg.org>
2012-12-02 17:25:09 -0800

    Should take care of the issues?

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,13 @@
 
 $plugin_root = dirname(__DIR__);
 
+// Initializes $CONFIG;
+require_once "$plugin_root/Elgg/engine/settings.example.php";
+
+// Bare minimum of engine needed to run tests
 require_once "$plugin_root/Elgg/engine/lib/elgglib.php";
+
+// Register auto-load classes for 1.9
 require_once "$plugin_root/Elgg/engine/lib/autoloader.php";
 
 elgg_register_classes("$plugin_root/classes");
