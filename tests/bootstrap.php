c694aacef568a9cfd03c23b251dc1a1b191c6113
Evan Winslow <evan@elgg.org>
2012-12-02 17:31:21 -0800

    Including masked dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,8 @@ require_once "$plugin_root/Elgg/engine/settings.example.php";
 // Bare minimum of engine needed to run tests
 require_once "$plugin_root/Elgg/engine/lib/elgglib.php";
 
-// Register auto-load classes for 1.9
-include_once "$plugin_root/Elgg/engine/lib/autoloader.php";
+// elgg_normalize_url required for ElggMenuItem
+require_once "$plugin_root/Elgg/engine/lib/output.php";
+
 
 elgg_register_classes("$plugin_root/classes");
