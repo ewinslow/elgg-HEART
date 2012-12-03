5d9947084c4d990283c0678a3ce8b07f5567f99d
Evan Winslow <evan@elgg.org>
2012-12-02 17:35:32 -0800

    Mocks elgg_normalize_url

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,8 +8,9 @@ require_once "$plugin_root/Elgg/engine/settings.example.php";
 // Bare minimum of engine needed to run tests
 require_once "$plugin_root/Elgg/engine/lib/elgglib.php";
 
-// elgg_normalize_url required for ElggMenuItem
-require_once "$plugin_root/Elgg/engine/lib/output.php";
-
+// Mocked elgg_normalize_url required for ElggMenuItem
+function elgg_normalize_url($url) {
+	return $url;
+}
 
 elgg_register_classes("$plugin_root/classes");
